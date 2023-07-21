e6d8ebe882a5c2f89fb9d025ebce5cfe14c282c2
alpha <pramadeishsm@gmail.com>
2023-07-21 12:12:46 +0530

    added context api for theme mode

--- src/components/DarkModeToggle/DarkModeToggle.jsx
+++ src/components/DarkModeToggle/DarkModeToggle.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import styles from './DarkModeToggle.module.css'
-const DarkModeToggle = () => {
-  const mode="dark"
-    return (
-    <div className={styles.container}>
-        <div className={styles.icon}>🌙</div>
-        <div className={styles.icon}>🔆</div>
-        <div className={styles.ball} style={mode==="light"?{left:"2px"}:{right:"2px"}}/>
-    </div>
-  )
-}
+"use client"
 
-export default DarkModeToggle
+import React, { useContext } from "react";
+import styles from "./darkModeToggle.module.css";
+import { ThemeContext } from "../../context/ThemeContext";
+
+const DarkModeToggle = () => {
+  const { toggle,mode } = useContext(ThemeContext);
+  return (
+    <div className={styles.container} onClick={toggle}>
+      <div className={styles.icon}>🌙</div>
+      <div className={styles.icon}>🔆</div>
+      <div
+        className={styles.ball}
+        style={mode === "light" ? { left: "2px" } : { right: "2px" }}
+      />
+    </div>
+  );
+};
+
+export default DarkModeToggle;
