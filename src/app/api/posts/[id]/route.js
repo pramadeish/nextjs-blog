826aa7bd9bdbd03af1678ccbe7b269e7f2c620d8
alpha <pramadeishsm@gmail.com>
2023-07-25 19:42:14 +0530

    added context api for posts

---
+++ src/app/api/posts/[id]/route.js
@@ -0,0 +1,16 @@
+import Post from "@/models/Post"
+import connect from "@/utils/db"
+import { NextResponse } from "next/server"
+
+export const GET = async(request,{params})=>{
+const {id}=params
+    try{
+await connect()
+const post=await Post.findById(id)
+return new NextResponse(JSON.stringify(post),{status:200})
+
+}catch(err){
+
+    return new NextResponse("Database Error!",{status:500})
+}
+}
--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,18 +1,40 @@
 import Image from 'next/image'
 import React from 'react'
 import styles from './page.module.css'
-const BlogPost = () => {
+import {notFound} from "next/navigation"
+
+async function getData(id) {
+  const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    return notFound()
+  }
+
+  return res.json();
+}
+
+// export async function generateMetadata({params}){
+//   const post=await getData(params._id)
+//   return{
+//     title:post.title,
+//   }
+// }
+
+const BlogPost = async ({params}) => {
+  const data=await getData(params.id)
   return (
     <div className={styles.container}>
       <div className={styles.top}>
         <div className={styles.info}>
-          <h1 className={styles.title}>testTitle</h1>
+          <h1 className={styles.title}>{data.title}</h1>
           <p className={styles.desc}>
-            test
+            {data.desc}
           </p>
           <div className={styles.author}>
             <Image
-              src="https://images.pexels.com/photos/17490216/pexels-photo-17490216/free-photo-of-road-landscape-summer-desert.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+            src={data.img}
               alt=""
               width={40}
               height={40}
@@ -23,8 +45,7 @@ const BlogPost = () => {
         </div>
         <div className={styles.imageContainer}>
           <Image
-            src="https://images.pexels.com/photos/17490216/pexels-photo-17490216/free-photo-of-road-landscape-summer-desert.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
-            alt=""
+          src={data.img}            alt=""
             fill={true}
             className={styles.image}
           />
@@ -32,7 +53,7 @@ const BlogPost = () => {
       </div>
       <div className={styles.content}>
         <p className={styles.text}>
-         test
+         {data.content}
         </p>
       </div>
     </div>
